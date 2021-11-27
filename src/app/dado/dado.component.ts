11a810f590d8f23df84d0a62e07f5b1132a402e6
anonimo223 <anguielugo44@gmail.com>
2021-11-27 12:40:38 -0500

    Lugo: Anexo de todo el segundo proyecto de Angular Dos

---
+++ src/app/dado/dado.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent implements OnInit {
+
+   @Input() valor: number;
+
+  constructor() { 
+    this.valor = 0;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
